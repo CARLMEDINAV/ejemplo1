acdb73afdc8556cd5000b7781d12c842af3c989d
CARLMEDINAV <137425914+CARLMEDINAV@users.noreply.github.com>
2023-06-27 12:33:30 -0400

    Codogo_comletado
    a

--- funciones.py
+++ funciones.py
@@ -1,26 +1,81 @@
-import numpy
+import numpy as np
 import colorama
-from colorama import Style,Fore,Back
+from colorama import Style, Fore, Back
+import os
+import msvcrt
+
+edificio = np.empty([10, 4], dtype=object)
+edificio[edificio == None] = "DISPONIBLE"
+
+clientes = []
+
+def limpiarpantalla():
+    print(Fore.RED + "<<<PRESIONE UNA TECLA PARA CONTINUAR>>>")
+    msvcrt.getch()
+    os.system("cls")
+
+def printcolor(texto):
+    print(Fore.LIGHTGREEN_EX + Style.BRIGHT + texto + Back.BLACK + Fore.RESET + Style.RESET_ALL)
 
 def printv(texto):
-    print(f"{Fore.GREEN}{Style.BRIGHT}{texto}{Back.BLACK}{Fore.RESET}{Style.RESET_ALL}")
+    print(Fore.GREEN + Style.BRIGHT + texto + Back.BLACK + Fore.RESET + Style.RESET_ALL)
 
 def printr(texto):
-    print(f"{Fore.RED}{Style.BRIGHT}{texto}{Back.BLACK}{Fore.RESET}{Style.RESET_ALL}")
+    print(Fore.RED + Style.BRIGHT + texto + Back.BLACK + Fore.RESET + Style.RESET_ALL)
 
-edificio=numpy.empty([10,4],object)
-
-def mostraredificio(edificio):
+def mostraredificio():
     print(edificio)
 
 def comprar():
     while True:
-        piso=int(input("Ingrese el piso que desea : "))
-        numero=int(input("Ingrese el numero de departamento : "))
-
-        if edificio[piso][numero]=="X":
-            printr(f"El edificio seleccionado ya se encuentra ocupado")
+        piso = int(input("Ingrese el piso que desea: "))
+        numero = int(input("Ingrese el numero de departamento: "))
+        
+        if piso >= 8:
+            precio = 200
+        elif piso < 8:
+            precio = 150
+        
+        rut = int(input("Ingrese su RUT para continuar con la compra: "))
+        nombre = input("Ingrese su Nombre: ")
+        
+        if edificio[piso][numero] == "VENDIDO":
+            piso += 1
+            numero += 1
+            printr("El edificio seleccionado ya se encuentra ocupado")
         else:
-            edificio[piso][numero]=="X"
-            mostraredificio(edificio)
-            printv(f"Edificio comprado exitosamente")
+            edificio[piso][numero] = "VENDIDO"
+            clientes.append({"piso": piso, "numero": numero, "rut": rut, "nombre": nombre})
+            mostraredificio()
+            printv("Edificio comprado exitosamente")
+            break
+
+def buscar_cliente(rut):
+    for cliente in clientes:
+        if cliente["rut"] == rut:
+            return cliente
+    return None
+
+def mostrar_clientes():
+    if len(clientes) > 0:
+        print("Lista de clientes:")
+        for cliente in clientes:
+            print(f"RUT: {cliente['rut']}, Nombre: {cliente['nombre']}, Piso: {cliente['piso']}, Número: {cliente['numero']}")
+    else:
+        printr("No hay clientes registrados.")
+
+def ganancias():
+    total_ganancias = 0
+    for i in range(10):
+        for j in range(4):
+            piso = edificio[i, j]
+            if piso == "VENDIDO":
+                if i >= 8:
+                    total_ganancias += 200
+                else:
+                    total_ganancias += 150
+    return total_ganancias
+
+def mostrar_ganancias():
+    total = ganancias()
+    printcolor(f"Ganancias totales: {total} Millones")
--- sistema.py
+++ sistema.py
@@ -1,10 +1,30 @@
 import funciones as fu
 
 while True:
- fu.printv(f"""
- 1) VER EDIFICIO 
- 2) COMPRAR EDIFICIO
- 3) BUSCAR DUEÑO
- 4) TOTAL DE GANACIAS
- 5) SALIR """)
- opcion=int(input("Seleccione : "))
+    fu.limpiarpantalla()
+    fu.printv("""
+    1) VER EDIFICIO
+    2) COMPRAR EDIFICIO
+    3) BUSCAR DUEÑO
+    4) TOTAL DE GANACIAS
+    5) SALIR
+    """)
+    opcion = int(input("Seleccione una opción: "))
+
+    if opcion == 5:
+        break
+    elif opcion == 1:
+        fu.mostraredificio()
+    elif opcion == 2:
+        fu.comprar()
+    elif opcion == 3:
+        rut = int(input("Ingrese el RUT del cliente a buscar: "))
+        cliente = fu.buscar_cliente(rut)
+        if cliente:
+            print(f"Cliente encontrado - RUT: {cliente['rut']}, Nombre: {cliente['nombre']}, Piso: {cliente['piso']}, Número: {cliente['numero']}")
+        else:
+            fu.printr("Cliente no encontrado.")
+    elif opcion == 4:
+        fu.mostrar_ganancias()
+    else:
+        fu.printr("Opcion NO VALIDA")
